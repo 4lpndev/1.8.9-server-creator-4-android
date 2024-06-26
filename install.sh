7ab2e5e2697855793082e75f404c7c6a6c364338
4lpndev <155200702+4lpndev@users.noreply.github.com>
2024-06-26 18:44:05 +0200

    fix termux-ngrok not deleting

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ git clone https://github.com/Yisus7u7/termux-ngrok
 
 cd termux-ngrok
 bash install.sh
-rm -rf termux-ngrok
 cd ..
+rm -rf termux-ngrok
 #install 1.8.9 server file
 curl https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar --output server.jar 
 
